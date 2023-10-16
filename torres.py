495c0341aa9b9c5ae24bd41febc42bca4d6b882a
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-10-16 11:18:48 +0200

    parte 1

---
+++ torres.py
@@ -0,0 +1,36 @@
+#Ejercicio de las torres de HAnoi.
+class NodoPila:
+    def __init__(self, valor):
+        self.valor = valor
+        self.siguiente = None
+
+class Pila:
+    def __init__(self):
+        self.tope = None
+
+    def push(self, valor):
+        nuevo_nodo = NodoPila(valor)
+        if not self.tope:
+            self.tope = nuevo_nodo
+        else:
+            nuevo_nodo.siguiente = self.tope
+            self.tope = nuevo_nodo
+
+    def pop(self):
+        if not self.tope:
+            return None
+        else:
+            valor = self.tope.valor
+            self.tope = self.tope.siguiente
+            return valor
+
+    def __str__(self):
+        if not self.tope:
+            return "Pila vacía"
+        else:
+            actual = self.tope
+            resultado = ""
+            while actual:
+                resultado += str(actual.valor) + "\n"
+                actual = actual.siguiente
+            return resultado.strip()
